7b25e1b6d48a283471b87e92a0703e097f769f2d
mulefish <pmontgomery@lululemon.com>
2024-05-04 10:22:58 -0700

    just getting started

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dispense_vending_machine
 Code for the dispense vending machine
+
+# TODOS: 
+1: Read actual input from the QR codes
+2: Test harness ( Restful end point ) 
+3: Serve a webpage that show the recent history of the vending machine and its current state ( password protected ) 
+4: Call GCP's DB to verify that it is OK to dispense a given spool 
+
---
+++ first.py
@@ -0,0 +1,28 @@
+import sys
+import requests
+
+def start_session():
+    response = requests.get('http://192.168.5.54:80/avend?action=start')
+    set_cookie_header = response.headers['set-cookie']
+    session_id = set_cookie_header.split(';')[0].split('=')[1]
+
+    print("Session started")
+    print("Session ID:", session_id)
+
+    return session_id
+
+def dispense_code(session_id):
+    headers = {
+        'Cookie': f'sessionid={session_id}'
+    }
+
+    response = requests.get('http://192.168.5.54:80/avend?action=dispense&code=14', headers=headers)
+    print("Dispense code successful")
+
+if __name__ == "__main__":
+    # Loop indefinitely to listen to standard input
+    for line in sys.stdin:
+        session_id = start_session()
+        if len(line.strip()) >0:
+                # Dispense code
+                dispense_code(session_id)
