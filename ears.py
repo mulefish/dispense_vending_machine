ff1363e72b3de683f827ded2ea948314b2ec5f5f
Paul Montgomery <finch.wren.swift@gmail.com>
2024-05-13 19:07:14 -0700

    it works on laptop

--- ears.py
+++ ears.py
@@ -1,8 +1,6 @@
 import sys
 import aiohttp
-import json
 from datetime import datetime
-import re
 import asyncio
 
 async def fetch_dummy_data():
@@ -26,33 +24,30 @@ async def dispense_code(session_id, spool):
         'Cookie': f'sessionid={session_id}'
     }
     command = f"http://192.168.5.54:80/avend?action=dispense&code={spool}"
+    print(command)
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(command) as response:
             print("Dispense code successful")
 
 async def main():
-    DEBUG = False
-    if DEBUG:
-        await fetch_dummy_data()
-        session_id = await start_session()
-        await dispense_code(session_id)
+    # DEBUG = False
+    # if DEBUG:
+    #     await fetch_dummy_data()
+    #     session_id = await start_session()
+    #     await dispense_code(session_id)
 
-    print("Loop indefinitely to listen to standard input")
+    print("Loop indefinitely to listen to standard input!")
     for line in sys.stdin:
         line = line.strip()
-        if line:
-            if line == "1,1,24":
-                print("Terminating loop due to signal 1,1,2")
-                break
-            else:
-                pieces = line.split(",")
-                if len(pieces) == 3:
-                    store, machine, spool = pieces
-                    await fetch_dummy_data()  # Fetch dummy data before starting session
-                    session_id = await start_session()
-                    await dispense_code(session_id, spool)
-                else:
-                    print("Invalid input format:", line)
+        pieces = line.split(",")
+        if len(pieces) == 3:
+            store, machine, spool = pieces
+            print("store {} machine {} spool {}".format( store, machine, spool))
+            await fetch_dummy_data()
+            session_id = await start_session()
+            await dispense_code(session_id, spool)
+        else:
+            print("Invalid input format:", line)
 
 if __name__ == "__main__":
     asyncio.run(main())
